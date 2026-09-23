ad9427cf17005614dfcc5ec802da6e7b90939343
Han Qiu <qiuhhan@gmail.com>
2026-09-23 10:04:50 -0600

    Implement POST API endpoint

---
+++ src/main/java/com/hanqiu/taskapp/TaskappApplication.java
@@ -0,0 +1,13 @@
+package com.hanqiu.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskappApplication.class, args);
+    }
+
+}
